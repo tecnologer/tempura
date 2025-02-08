73400752d5bfd06422f168c85c0bfd444581e7c2
Rey David Dominguez Soto <rdominguez@tecnologer.net>
2025-02-08 14:29:14 -0700

    read battery percentage

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -5,6 +5,10 @@
 #include <Wire.h>
 
 #define LIQUID_SENSOR_PIN D5  // GPIO14
+#define R1 33000.0            // Resistor R1 = 33kΩ
+#define R2 10000.0            // Resistor R2 = 10kΩ
+#define MAX_ADC 1023.0        // 10-bit ADC resolution
+#define REF_VOLTAGE 3.3       // ESP8266 ADC max voltage is 1V
 
 const char* ssid = "<WIFI_SSID>";
 const char* password = "<WIFI_PASSWORD>";
@@ -12,8 +16,12 @@ const char* password = "<WIFI_PASSWORD>";
 //Your Domain name with URL path or IP address with path
 const char* serverName = "<URL>";
 
-const int EMPTY_TANK = 0 ;
+const int EMPTY_TANK = 0;
 const int FULL_TANK = 100;
+const float FULL_BAT_LEVEL = 4.4;
+const float EMPTY_BAT_LEVEL = 3.4;
+const float DANGER_BAT_LEVEL = 3.3;
+
 
 unsigned long timerDelay = 30000;
 
@@ -28,13 +36,17 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  
+
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 
   Serial.println("Timer set to " + String(timerDelay / 1000) + " seconds (timerDelay variable), it will take " + String(timerDelay / 1000) + " seconds before publishing the first reading.");
   pinMode(LIQUID_SENSOR_PIN, INPUT_PULLUP);  // Use internal pull-up resistor
+}
+
+float calculateBatPercentage(float batLevel) {
+  return ((batLevel - EMPTY_BAT_LEVEL) / (FULL_BAT_LEVEL - EMPTY_BAT_LEVEL)) * 100;
 }
 
 void loop() {
@@ -46,12 +58,25 @@ void loop() {
   float temperature = sht30.fTemp;
   float celciusTemperature = ((temperature - 32) * 5) / 9;
   int fluidLevel = EMPTY_TANK;
-  
-  if(digitalRead(LIQUID_SENSOR_PIN) == LOW) {
+
+  if (digitalRead(LIQUID_SENSOR_PIN) == LOW) {
     fluidLevel = FULL_TANK;
   }
 
-  sprintf(buffer, "Temperature: %.2f°C (%.2f°F), Humidity: %.1f%%, Fluid Level: %d%%", celciusTemperature, temperature, humidity, fluidLevel);
+  int adcValue = analogRead(A0);
+  float voltageAtA0 = (adcValue / MAX_ADC) * REF_VOLTAGE;  // Voltage at A0
+
+  // Calculate actual battery voltage based on resistor divider
+  float batteryVoltage = voltageAtA0 * ((R1 + R2) / R2);
+  float batPercentage = calculateBatPercentage(batteryVoltage);
+
+  Serial.print("Battery Voltage: ");
+  Serial.print(batteryVoltage);
+  Serial.println(" V");
+  Serial.print("Read value: ");
+  Serial.println(adcValue);                                                                                     
+
+  sprintf(buffer, "Temperature: %.2f°C (%.2f°F), Humidity: %.1f%%, Fluid Level: %d%%, Battery Level: %.2f%%", celciusTemperature, temperature, humidity, fluidLevel, batPercentage);
   Serial.println(buffer);
 
   //Check WiFi connection status
@@ -67,7 +92,7 @@ void loop() {
 
     // If you need an HTTP request with a content type: application/json, use the following:
     http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST("{\"temperature\": " + String(celciusTemperature) + ",\"humidity\": " + String(humidity) + ", \"fluid_level\": " + String(fluidLevel) + ", \"bat_level\": 0}");
+    int httpResponseCode = http.POST("{\"temperature\": " + String(celciusTemperature) + ",\"humidity\": " + String(humidity) + ", \"fluid_level\": " + String(fluidLevel) + ", \"bat_level\": " + String(batPercentage) + "}");
 
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
