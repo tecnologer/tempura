6e55cd50f828d67b37734a8a96eddb5f401b99d3
tecnologer <rdominguez@tecnologer.net>
2025-02-09 20:21:48 -0600

    add env var telegram bot token

--- pkg/contants/envvarname/env_var_name.go
+++ pkg/contants/envvarname/env_var_name.go
@@ -1,11 +1,12 @@
 package envvarname
 
 const (
-	DBHost     = "TEMPURA_DB_HOST"
-	DBPort     = "TEMPURA_DB_PORT"
-	DBUsername = "TEMPURA_DB_USERNAME"
-	DBPassword = "TEMPURA_DB_PASSWORD" //nolint:gosec // This is not a real password
-	DBName     = "TEMPURA_DB_NAME"
-	DBSSLMode  = "TEMPURA_DB_SSL_MODE"
-	APIPort    = "TEMPURA_API_PORT"
+	DBHost           = "TEMPURA_DB_HOST"
+	DBPort           = "TEMPURA_DB_PORT"
+	DBUsername       = "TEMPURA_DB_USERNAME"
+	DBPassword       = "TEMPURA_DB_PASSWORD" //nolint:gosec // This is not a real password
+	DBName           = "TEMPURA_DB_NAME"
+	DBSSLMode        = "TEMPURA_DB_SSL_MODE"
+	APIPort          = "TEMPURA_API_PORT"
+	TelegramBotToken = "TEMPURA_TELEGRAM_BOT_TOKEN"
 )
